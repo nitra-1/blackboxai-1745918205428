8e73a8ee2428712a7c2dd22788718ca0fceb9307
nitra-1 <ranenitin@gmail.com>
2025-04-29 14:47:11 +0530

    Add leasingstore-backend/src/main/java/com/leasingstore/entity/Message.java 1745918230576

---
+++ leasingstore-backend/src/main/java/com/leasingstore/entity/Message.java
@@ -0,0 +1,76 @@
+package com.leasingstore.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+/**
+ * Entity class representing a Message between User and Provider
+ */
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User sender;
+
+    @ManyToOne
+    private User receiver;
+
+    private String content;
+
+    private LocalDateTime sentAt;
+
+    // Constructors, getters, setters
+
+    public Message() {}
+
+    public Message(User sender, User receiver, String content, LocalDateTime sentAt) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+        this.sentAt = sentAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(LocalDateTime sentAt) {
+        this.sentAt = sentAt;
+    }
+}
